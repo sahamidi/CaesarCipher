4ccc79d8b4dc1ea1a311c9a7117e24deb19ec552
Sal Hamidi <hamidisa87@gmail.com>
2020-06-02 14:20:02 -0400

    Add notes to if functions

--- cipher.js
+++ cipher.js
@@ -13,25 +13,27 @@ var leng = (orgText.length - 1)
 arr = []
 i = 0
 while (i <= leng) {
-    newTxt = orgText.codePointAt(i)+shiftFact
+    newTxt = orgText.codePointAt(i)+shiftFact  //Take shift factor and add number code for letter
+    if (orgText>122 || orgText < 65 || (orgText > 90 && orgText < 97)) {
+        arr.push(orgText.codePointAt(i))
+    }    
     if (newTxt > 122) {
-        arr.push((newTxt-122)+96)
-    }
+        arr.push((newTxt-122)+96)              //If number code is greater than end of alphabet for upercase
+    }                                          //Wrap around to beginning of lowercase alphabet 
     else if (newTxt > 90 && newTxt < 97) {
-        arr.push((newTxt-90)+64)
-    }
-    else if (shiftFact < 0 && newTxt < 97 && newTxt > 90) {
-        arr.push((newTxt-97)+123)
-    }
+        arr.push((newTxt-90)+64)                //If number code is greater than end of alphabet for upercase
+    }                                           //Wrap around to beginning of upercase alphabet     
+    else if (shiftFact < 0 && newTxt < 97 && newTxt > 90) { 
+        arr.push((newTxt-97)+123)               //if shift factor is negative, and initial text and shift factor
+    }                                           //puts code outside of alphabet range, then wrap around to end of 
+                                                //alphabet going towards negatvie direction for lowercase
     else if (shiftFact<0 && newTxt < 65) {
-        arr.push((newTxt-65)+91)
-    }   
-
+        arr.push((newTxt-65)+91)                //if shift factor is negative, and initial text and shift factor
+    }                                           //puts code outside of alphabet range, then wrap around to end of 
+                                                //alphabet going towards negatvie direction for upercase
     else {
         arr.push(orgText.codePointAt(i)+shiftFact)
     }
-
- 
     console.log(arr)
 i++
 }
