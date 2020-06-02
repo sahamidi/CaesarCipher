60e21c67aa628e64f72f986ca2aa6c174f6224d1
Sal Hamidi <hamidisa87@gmail.com>
2020-06-02 13:57:03 -0400

    Add if functions for new array

--- cipher.js
+++ cipher.js
@@ -1,5 +1,5 @@
 
-var shiftFact = document.getElementById("shiftFact")
+
 
 
 
@@ -8,13 +8,31 @@ function ()
 {
 
 var orgText = document.getElementById("orgText").value
+var shiftFact = Number(document.getElementById("shiftFact").value)
 var leng = (orgText.length - 1)
 arr = []
 i = 0
 while (i <= leng) {
-    orgText.charCodeAt(i)
-arr.push(orgText.codePointAt(i))
-console.log(arr)
+    newTxt = orgText.codePointAt(i)+shiftFact
+    if (newTxt > 122) {
+        arr.push((newTxt-122)+96)
+    }
+    else if (newTxt > 90 && newTxt < 97) {
+        arr.push((newTxt-90)+64)
+    }
+    else if (shiftFact < 0 && newTxt < 97 && newTxt > 90) {
+        arr.push((newTxt-97)+123)
+    }
+    else if (shiftFact<0 && newTxt < 65) {
+        arr.push((newTxt-65)+91)
+    }   
+
+    else {
+        arr.push(orgText.codePointAt(i)+shiftFact)
+    }
+
+ 
+    console.log(arr)
 i++
 }
 })
